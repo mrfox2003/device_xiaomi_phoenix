01d0f948efba2dd64a87b0442ff545089e862c55
SonalSingh18 <sonal.singh.19993@gmail.com>
2020-11-24 01:43:42 +0000

    phoenix: Add GoogleCameraGo Signed-off-by: Niranjan BR <niranjankannan2003@gmail.com>

--- device.mk
+++ device.mk
@@ -161,6 +161,10 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor \
     android.hardware.keymaster@4.0.vendor
 
+#Gcam Go
+PRODUCT_PACKAGES += \
+    GoogleCameraGo
+
 # CNE
 PRODUCT_PACKAGES += \
     cneapiclient \
